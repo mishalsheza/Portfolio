c80fa108280140afc6355f662d56a90546f86f59
shezamishal19 <sheza.mishal@mail.com>
2024-06-07 12:53:01 +0530

    made changes in the text

--- src/portfolio.js
+++ src/portfolio.js
@@ -36,7 +36,7 @@ function Portfolio() {
           <Link to="experience" smooth={true} duration={500} offset={-100} className="nav-link">Experience</Link>
           <Link to="profile" smooth={true} duration={500} offset={-100} className="nav-link">Profile</Link>
           <Link to="portfolio" smooth={true} duration={500} offset={-100} className="nav-link">Portfolio</Link>
-          <Link to="clients" smooth={true} duration={500} offset={-100} className="nav-link">Clients</Link>
+          {/* <Link to="clients" smooth={true} duration={500} offset={-100} className="nav-link">Clients</Link> */}
           <Link to="contact" smooth={true} duration={500} offset={-100} className="nav-link">Contact</Link>
         </div>
       </nav>
@@ -51,8 +51,6 @@ function Portfolio() {
             <br />
             <span>MISHAL</span>
           </h1>
-          <p>UI/UX DESIGNER AND WEB DEVELOPER</p>
-          <button className="resume-button">Download Resume</button>
         </section>
       </div>
       <h2 class="title">About Me</h2>
@@ -67,25 +65,28 @@ function Portfolio() {
             experiences that meet both user needs and business objectives.
           </h3>
           <p className='p2'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem officiis harum quam laboriosam, 
-          error cum facere, cumque beatae voluptate omnis quia tempore alias ipsum incidunt nemo inventore expedita esse distinctio!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem officiis harum quam laboriosam, 
-          error cum facere, cumque beatae voluptate omnis quia tempore alias ipsum incidunt nemo inventore expedita esse distinctio!
+
+          I am Sheza Mishal, an Undergraduate Student studying in scaler school of Technology. With a passion for Computer Science.
+          My journey has been shaped by buliding projects like web scraper,cloning websites 
+          which have equipped me with Development Skills.In my free time, I enjoy Reading Books and writing Poetry, finding inspiration in 
+          reading Books like the reader's digest.I believe in the power of Dedication and hardwork.
+         Feel free to connect with me to discuss any projects in development. 
+         Together, let's Build.
           </p>
           <hr className="divider" />
         <div className="contact-info">
           <span className="phone">
             <strong>Phone: </strong>
             <br></br>
-            123-456-7890</span>
+            +91 9025233043</span>
           <span className="email">
           <strong>Email: </strong>
             <br></br>
-            sheza@example.com</span>
+            sheza@email.com</span>
           <span className="website">
           <strong>Website:</strong>
             <br></br>
-            www.brownsine.com</span>
+            www.sheza.com</span>
         </div>
           </div>
         
@@ -102,7 +103,7 @@ function Portfolio() {
 
             <div class="educontainer up-education">
               <div class="edu-text">
-                <h3>2008 - 2010</h3>
+                <h3>2026 - 2027</h3>
                 <h4>Master of Computer Science</h4>
                 <img src={DotIcon} alt="Dot Icon"></img>
 
@@ -111,7 +112,7 @@ function Portfolio() {
 
           <div class="educontainer up-education">
               <div class="edu-text">
-                <h3>2004 - 2008</h3>
+                <h3>2023 - 2025</h3>
                 <h4>Bachelor of Computer Science</h4>
                 <img src={DotIcon} alt="Dot Icon"></img>
 
@@ -119,8 +120,8 @@ function Portfolio() {
           </div>
           <div class="educontainer up-education">
               <div class="edu-text">
-                <h3>2004 - 2008</h3>
-                <h4>Bachelor of Creative Design</h4>
+                <h3>2020 - 2022</h3>
+                <h4>High School</h4>
                 <img src={DotIcon} alt="Dot Icon"></img>
 
             </div>
@@ -130,11 +131,13 @@ function Portfolio() {
 
           <div class="educontainer down-education">
         <div class="edu-text">
-          <h3>UNIVERSITY OF NORTH CAROLINA</h3>
-          <p> North Carolina, USA</p>
+          <h3>WOOLF UNIVERSITY </h3>
+          <p> Banglore, Karnataka</p>
           <p>
-            Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore
-            eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.
+          My pursuit of a master's degree has been a pivotal step in my academic and professional journey, allowing me to delve deeper into my chosen field and refine my expertise.
+          At Woolf University, I pursued a Master in Computer Sience. This program offered a comprehensive curriculum that combined 
+          theoretical knowledge with practical application,
+          providing me with a well-rounded understanding of Computer Science and its intricacies.
              </p>         
         </div>
       </div>
@@ -143,11 +146,12 @@ function Portfolio() {
         
           <div class="educontainer down-eduaction">
         <div class="edu-text">
-          <h3>UNIVERSITY OF NORTH CAROLINA</h3>
-          <p> North Carolina, USA</p>
+          <h3>SCALER SCHOOL OF TECHNOLOGY</h3>
+          <p> Banglore, Karnataka</p>
           <p>
-            Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore
-            eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.
+          Upon graduating from high school, I embarked on the next chapter of my academic journey at Scaler School of Technology,
+           where I pursued a degree in Bachelor in Computer Science. Here, I immersed myself in a diverse range of courses, honing my critical thinking abilities, 
+           expanding my knowledge base, and engaging with like-minded peers and mentors. 
              </p>         
         </div>
       </div>
@@ -157,11 +161,12 @@ function Portfolio() {
 
           <div class="educontainer down-education">
         <div class="edu-text">
-          <h3>UNIVERSITY OF BOLTON</h3>
-          <p> Bolton, United Kingdome</p>
+          <h3>WISDOM PARK INTERNATIONAL SCHOOL</h3>
+          <p> Vellore,Tamil Nadu</p>
           <p>
-            Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore
-            eu fugiat nulla pari. Excepteur sint occana inna tecat cupidatat non proident.
+          My educational journey began with Wisdom Park INternational, where I cultivated the fundamental skills and curiosity 
+          that laid the groundwork for future academic pursuits. Building upon this foundation,
+            I delved deeper into subjects of interest and began to explore my passions.
              </p>         
         </div>
       </div>
@@ -182,7 +187,7 @@ function Portfolio() {
       <div className='skill-container'>
         <div className='skill-column'>
         <div className='skill-box'>
-        <span className='skill-title'>ADOBE PHOTOSHOP</span>
+        <span className='skill-title'>JAVA</span>
 
           <div className='skill-bar'>
               <span className='skill-per'>
@@ -192,7 +197,7 @@ function Portfolio() {
         </div>
 
         <div className='skill-box'>
-        <span className='skill-title'>ADOBE ILLUSTRATOR</span>
+        <span className='skill-title'>PYTHON</span>
 
           <div className='skill-bar'>
               <span className='skill-per'>
@@ -202,7 +207,7 @@ function Portfolio() {
         </div>
 
         <div className='skill-box'>
-        <span className='skill-title'>ADOBE AFTER EFFECTS</span>
+        <span className='skill-title'>HTML</span>
 
           <div className='skill-bar'>
               <span className='skill-per'>
@@ -211,7 +216,7 @@ function Portfolio() {
               </div>
         </div>
         <div className='skill-box'>
-        <span className='skill-title'>SKETCH</span>
+        <span className='skill-title'>CSS</span>
 
           <div className='skill-bar'>
               <span className='skill-per'>
@@ -222,7 +227,7 @@ function Portfolio() {
       </div>
       <div className='skill-column'>
         <div className='skill-box'>
-        <span className='skill-title'>HTML 5</span>
+        <span className='skill-title'>JS</span>
 
           <div className='skill-bar'>
               <span className='skill-per'>
@@ -232,7 +237,7 @@ function Portfolio() {
         </div>
 
         <div className='skill-box'>
-        <span className='skill-title'>CSS 3 ANIMATION</span>
+        <span className='skill-title'>REACT</span>
 
           <div className='skill-bar'>
               <span className='skill-per'>
@@ -442,7 +447,7 @@ function Portfolio() {
       </div>
     </Element>
 
-    <Element name='clients' className='section'>
+    {/* <Element name='clients' className='section'>
       <h1 className='client-text'>CLIENTS</h1>
       <hr className="divider" />
 
@@ -476,7 +481,7 @@ function Portfolio() {
       </div>
       <hr className="divider" />
 
-    </Element>
+    </Element> */}
 
     <Element name='contact' className='contact'>
       <h1 className='contact-text'>CONTACT</h1>
@@ -501,7 +506,6 @@ function Portfolio() {
         <div className='contact-info'>
           <div className='contact-info-box'>
               <h2>SHEZA MISHAL</h2>
-              <h3>UI/UX DESIGNER</h3>
             </div>
           </div>
           <div className='contact-info-col'>
@@ -510,11 +514,11 @@ function Portfolio() {
           </div>
           <div className='contact-info-col'>
             <h3>Email</h3>
-            <h4>browny@info.com</h4>
+            <h4>sheza@email.com</h4>
           </div>
           <div className='contact-info-col'>
             <h3>Website</h3>
-            <h4>www.brownsine.com</h4>
+            <h4>www.sheza.com</h4>
         </div>
         </div>
       </div>
